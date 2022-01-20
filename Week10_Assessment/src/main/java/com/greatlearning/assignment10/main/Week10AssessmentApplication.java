45da48cd0a88f2a2307e88beae23a5456b852ac0
Sunny281 <sunnychourasiya2001@gmail.com>
2022-01-20 17:24:32 +0530

    assignment10

---
+++ Week10_Assessment/src/main/java/com/greatlearning/assignment10/main/Week10AssessmentApplication.java
@@ -0,0 +1,14 @@
+package com.greatlearning.assignment10.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week10AssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week10AssessmentApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
---
+++ Week10_Assessment/src/test/java/com/greatlearning/assignment10/main/Week10AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatlearning.assignment10.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week10AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
